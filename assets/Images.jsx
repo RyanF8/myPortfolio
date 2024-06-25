10ae9b775ed78f5ec06ee7b86cd48f83b5e061d5
RyanF8 <Ryanfish4229@gmail.com>
2024-06-25 19:09:32 -0400

    fixed images finally

--- assets/Images.jsx
+++ assets/Images.jsx
@@ -1,5 +1,7 @@
 const Images = {
     image1 : new URL("../assets/DSC00584.JPG", import.meta.url).href,
+    jeopardyImg : new URL("../assets/Jeopardy-screenshot.png", import.meta.url).href,
+    reactChatImg : new URL("../assets/React-chat-screenshot.png", import.meta.url).href
 }
 
 export default Images
