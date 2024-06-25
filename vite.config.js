5f0f1ba569c1c1ffe0e6a948ec18edbc68471f06
RyanF8 <Ryanfish4229@gmail.com>
2024-06-25 18:52:59 -0400

    configured file changed

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './dist/index.html',
+        assets: './assets'
+      }
+    }
+  }
 })
+
+
